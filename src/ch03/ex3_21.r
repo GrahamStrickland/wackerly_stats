d7d63f43495c8fc25aa17cfdde28dd2f45ee3a52
Graham Strickland <grahamhstrickland@gmail.com>
2024-12-01 10:26:09 +0200

    Add simplified script for ex3_21.r

--- src/ch03/ex3_21.r
+++ src/ch03/ex3_21.r
@@ -6,16 +6,10 @@ c <- 8
 r <- 21:26
 p <- c(.05, .2, .3, .25, .15, .05)
 
+# Compute N(r) = C * pi * r^2
+N <- c * pi * r^2
 
-n <- function(c, r, p) {
-  return(c * pi * r^2 * p)
-}
-
-
-mu <- 0
-for (i in seq_along(r)) {
-  mu <- mu + n(c, r[i], p[i])
-}
-
+# Compute mu = sum(p(r) * N(r))
+mu <- sum(p * N)
 
 print(paste0("mu = ", mu))
