fbd89eaef3e1cf020b9e2047150684d08c17a87e
Graham Strickland <grahamhstrickland@gmail.com>
2025-03-02 15:27:19 +0200

    Add histogram plot for ex3.47

---
+++ src/ch03/ex3_47.r
@@ -0,0 +1,28 @@
+# WMS Ex. 3.47 - Histogram representing binomial probability
+# distribution for n = 20, p = .5
+
+
+require("latex2exp")
+
+
+ys <- c(
+  .000, .000, .000, .001, .006,
+  .021, .058, .132, .252, .412,
+  .588, .748, .868, .942, .979,
+  .994, .999, 1.000, 1.000, 1.000
+)
+xs <- 1:20
+
+pdf("ex3_47.pdf")
+
+barplot(
+  ys,
+  space = 0,
+  main = TeX(r"(Binomial probability histogram for $n = 20$, $p = .5$)"),
+  xlab = TeX(r"($y$)"),
+  names.arg = xs,
+  ylab = TeX(r"($p(y)$)"),
+)
+
+
+dev.off()
